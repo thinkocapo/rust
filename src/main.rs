366ef61ce95ad50f265cfa4bd324b726b6851fe2
William Capozzoli <thinkocapo@gmail.com>
2020-07-01 16:23:05 -0700

    config headers

--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,28 @@ fn main() {
     let _guard = sentry::init("https://92f3ece3dfb04b978f5de29777e456ee@o87286.ingest.sentry.io/5259785");
     env::set_var("RUST_BACKTRACE", "1");
 
+    // NEW 07/01/20
+    let app = server::new(|| { App::new()
+        .configure(|app| Cors::for_app(app) // <- Construct CORS middleware builder
+            // .allowed_origin("https://www.rust-lang.org/")
+            .allowed_origin("http://localhost:5000/") // React runs on 5000
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT, ])
+            .allowed_header(header::CONTENT_TYPE)
+            .max_age(3600)
+            // .middleware(SentryMiddleware::new()) // fails
+            .resource("/index.html", |r| {
+                r.method(http::Method::GET).f(|_| HttpResponse::Ok());
+                r.method(http::Method::HEAD).f(|_| HttpResponse::MethodNotAllowed());
+            })
+            .resource("/handled", |r| r.f(handled))
+            .resource("/checkout", |r| r.f(checkout))
+            .register())
+    })
+    .bind("127.0.0.1:3001")
+    .unwrap()
+    .run();
+
     // OLD 06/30/20
     // let _cors = cors::Cors::build()
     // .allowed_origin("127.0.0.1")
@@ -69,29 +91,6 @@ fn main() {
     // .bind("127.0.0.1:3001")
     // .unwrap()
     // .run();
-
-    // NEW 07/01/20
-    let app = server::new(|| { App::new()
-        .configure(|app| Cors::for_app(app) // <- Construct CORS middleware builder
-            // .allowed_origin("https://www.rust-lang.org/")
-            // .allowed_origin("127.0.0.1")
-            .allowed_origin("http://localhost:5000/") // React runs on 5000
-            .allowed_methods(vec!["GET", "POST"])
-            .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
-            .allowed_header(header::CONTENT_TYPE)
-            .max_age(3600)
-            // .middleware(SentryMiddleware::new()) // fails
-            .resource("/index.html", |r| {
-                r.method(http::Method::GET).f(|_| HttpResponse::Ok());
-                r.method(http::Method::HEAD).f(|_| HttpResponse::MethodNotAllowed());
-            })
-            .resource("/handled", |r| r.f(handled))
-            .resource("/checkout", |r| r.f(checkout))
-            .register())
-    })
-    .bind("127.0.0.1:3001")
-    .unwrap()
-    .run();
 }
 
 // .middleware(SentryMiddleware::builder().emit_header(true).finish())
